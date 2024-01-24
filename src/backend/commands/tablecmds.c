cd83815137b7daa94b5ae5d8c55ece12adb8696a
Michael Paquier <michael@paquier.xyz>
2024-01-24 14:20:13 +0900

    Fix ALTER TABLE .. ADD COLUMN with complex inheritance trees
    This command, when used to add a column on a parent table with a complex
    inheritance tree, tried to update multiple times the same tuple in
    pg_attribute for a child table when incrementing attinhcount, causing
    failures with "tuple already updated by self" because of a missing
    CommandCounterIncrement() between two updates.
    
    This exists for a rather long time, so backpatch all the way down.
    
    Reported-by: Alexander Lakhin
    Author: Tender Wang
    Reviewed-by: Richard Guo
    Discussion: https://postgr.es/m/18297-b04cd83a55b51e35@postgresql.org
    Backpatch-through: 12

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -6376,6 +6376,10 @@ ATExecAddColumn(List **wqueue, AlteredTableInfo *tab, Relation rel,
 							colDef->colname, RelationGetRelationName(rel))));
 
 			table_close(attrdesc, RowExclusiveLock);
+
+			/* Make the child column change visible */
+			CommandCounterIncrement();
+
 			return InvalidObjectAddress;
 		}
 	}
